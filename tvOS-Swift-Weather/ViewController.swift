4d1e1f7208c7e5e5e484d436e37637c2a00bda69
Pooya Hatami <pooya@node-1w7jr9qro06sw04hcipbybhw2.ipv6.telus.net>
2018-08-15 15:58:30 -0700

    done

--- tvOS-Swift-Weather/ViewController.swift
+++ tvOS-Swift-Weather/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var currentSummary: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url1 = "https://api.forecast.io/forecast/YOURSECRETCODE/37.7833,122.4167"
+        
+        if let url = NSURL(string: url1) {
+            if let data = NSData(contentsOf: url as URL){
+                do {
+                    let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments)
+                    
+                    let newDict : NSDictionary = (parsed as? NSDictionary)!
+                    let newDictCurrently : NSDictionary = newDict["currently"]! as! NSDictionary
+                    let newDictSummary  : NSString = newDictCurrently[
+                        "summary"] as! NSString
+                    let newDictTemperature  : NSNumber = newDictCurrently[
+                        "temperature"] as! NSNumber
+                        print(newDictSummary)
+                        print(newDictTemperature)
+                    self.currentSummary.text = newDictSummary as String
+                    self.currentTemp.text = newDictTemperature.stringValue
+                    
+                }
+                catch let error as NSError {
+                    print("A JSON parsithng error occurred, here are the details:\n \(error)")
+                }
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
